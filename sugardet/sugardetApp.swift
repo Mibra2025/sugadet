18d9221faadd67bddfba4d5aa21a194fd7f844dc
Mibra2025 <Mibra2025@yahoo.com>
2025-09-20 15:02:38 +0300

    Initial Commit

---
+++ sugardet/sugardetApp.swift
@@ -0,0 +1,17 @@
+//
+//  sugardetApp.swift
+//  sugardet
+//
+//  Created by m on 20.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct sugardetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sugardetTests/sugardetTests.swift
@@ -0,0 +1,17 @@
+//
+//  sugardetTests.swift
+//  sugardetTests
+//
+//  Created by m on 20.09.2025.
+//
+
+import Testing
+@testable import sugardet
+
+struct sugardetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
